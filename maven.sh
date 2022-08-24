8d5fdb32238988b5aeb7ddbfdf3cc64468585e2c
Praveen rachamreddy <43966628+praveenrachamreddy@users.noreply.github.com>
2022-08-25 00:58:51 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,9 +1,10 @@
-sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
-sudo yum install -y java-1.8.0-devel
-sudo /usr/sbin/alternatives --config java 
-sudo /usr/sbin/alternatives --config javac
+#sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+#sudo yum install -y apache-maven
+#sudo yum install -y java-1.8.0-devel
+#sudo /usr/sbin/alternatives --config java 
+#sudo /usr/sbin/alternatives --config javac
 ------------
-# in amazon linux we can use this directly if java is there
-# sudo yum install -y maven
+sudo amazon-linux-extras install -y java-openjdk11
+sudo yum install -y maven
+mvn -version
