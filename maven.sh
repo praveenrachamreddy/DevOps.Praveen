52813536aa44d2e6a7744637e00d6aaa1af7370a
Praveen rachamreddy <43966628+praveenrachamreddy@users.noreply.github.com>
2022-08-24 21:07:40 +0530

    create maven.sh
    created maven script

---
+++ maven.sh
@@ -0,0 +1,9 @@
+sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+sudo yum install java-1.8.0-devel
+sudo /usr/sbin/alternatives --config java 
+sudo /usr/sbin/alternatives --config javac
+------------
+# in amazon linux we can use this directly if java is there
+# sudo yum install -y maven
