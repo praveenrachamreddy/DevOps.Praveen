fb2c6e380d116b7069f117669c5036c5337792e6
Praveen rachamreddy <43966628+praveenrachamreddy@users.noreply.github.com>
2022-08-24 21:12:39 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,7 +1,7 @@
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
-sudo yum install java-1.8.0-devel
+sudo yum install -y java-1.8.0-devel
 sudo /usr/sbin/alternatives --config java 
 sudo /usr/sbin/alternatives --config javac
 ------------
