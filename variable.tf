12ddc240ae7cb63760bde498549feb406043b689
Praveen rachamreddy <43966628+praveenrachamreddy@users.noreply.github.com>
2022-08-25 23:39:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "cidr" {}
+variable "region" {}
+variable "pub_cidr" {}
+variable "azs" {}
+variable "pvt_cidr" {}
+variable "data_cidr" {}
+variable "myip" {}
+variable "ami" {}
+variable "type" {}
