e11879dc06662b4bf38872d23e78f388289986f3
Praveen rachamreddy <43966628+praveenrachamreddy@users.noreply.github.com>
2022-08-26 13:54:44 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,8 +11,62 @@ output "bastion"{
 output "keyfile"{
   value = aws_key_pair.keyfile.id
 }
-output ""{
-  value=
+output "pubsubnet"{
+  value= aws_subnet.pubsubnet.id
+}
+output "pvtsubnet"{
+  value= aws_subnet.pvtsubnet.id
+}
+output "datasubnet"{
+  value= aws_subnet.datasubnet.id
+}
+output "igw"{
+  value= aws_internet_gateway.igw.id
+}
+output "eip"{
+  value= aws_eip.eip.id
+}
+output "natgw"{
+  value= aws_nat_gateway.natgw.id 
+}
+output "pubroute"{
+  value= aws_route_table.pubroute.id
+}
+output "pvtroute"{
+  value= aws_route_table.pvtroute.id
+}
+output "dataroute"{
+  value= aws_route_table.dataroute.id
+}
+output "pubassos"{
+  value= aws_route_table_association.pubassos.id
+}
+output "pvtassos"{
+  value= aws_route_table_association.pvtassos.id
+}
+output "dataassos"{
+  value= aws_route_table_association.dataassos.id
+}
+output "alb-sg"{
+  value= aws_security_group.alb-sg.id
+}
+output "alb"{
+  value= aws_lb.alb.id
+}
+output "tomcat-sg"{
+  value= aws_lb_target_group.tomcat-sg.id
+}
+output "test"{
+  value= aws_lb_target_group_attachment.test.id
+}
+output "front_end"{
+  value= aws_lb_listener.front_end.id
+}
+output "tomcat-sg"{
+  value= aws_security_group.tomcat-sg.id
+}
+output "tomcat"{
+  value= aws_instance.tomcat.id
 }
 output ""{
   value=
